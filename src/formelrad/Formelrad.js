e55edbae61eeb9a29cc387eed8a46c53e0bac781
Cyril Tobler <cyriltoibler@icloud.com>
2025-06-15 20:47:08 +0200

    A message field is  required.

--- src/formelrad/Formelrad.js
+++ src/formelrad/Formelrad.js
@@ -8,7 +8,8 @@ export default function Formelrad() {
         u: 10,
         i: 2,
         r: "",
-        p: ""
+        p: "",
+        message: ""
     })
 
     const handleSubmit = (event) => {
@@ -26,6 +27,18 @@ export default function Formelrad() {
             /*calculate u and p */
             setValues(values => ({...values, u: values.i * values.r}));
             setValues(values => ({...values, p: values.i * values.i * values.r}));
+        } else if (values.i === "" && values.r === "") {
+            /*calculate i and r */
+            setValues(values => ({...values, i: values.p / values.u}));
+            setValues(values => ({...values, r: values.u * values.u / values.p}));
+        } else if (values.i === "" && values.p === "") {
+            /*calculate i and p */
+            setValues(values => ({...values, i: values.u / values.r}));
+            setValues(values => ({...values, p: values.u * values.u / values.r}));
+        } else {
+            /*calculate r and p */
+            setValues(values => ({...values, r: values.u / values.i}));
+            setValues(values => ({...values, p: values.u * values.i}));
         }
     }
 
@@ -42,6 +55,7 @@ export default function Formelrad() {
                     <InputField color={"black"} value={values.r} label="Widerstand" handleChange={e => {setValues(values => ({...values, r: e.target.value}))}} />
                     <InputField color={"black"} value={values.p} label="Leistung" handleChange={e => {setValues(values => ({...values, p: e.target.value}))}} />
                     <button type="submit">Calculate</button>
+                    <p>{values.message}</p>
                 </form>
             </section>
         </>
